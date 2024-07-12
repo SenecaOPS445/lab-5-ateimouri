fe309da5a4ccf08c0541fc8a48bf374c0589f5fb
Ali Teimouri <ateimouri1@myseneca.ca>
2024-07-12 11:09:46 -0400

    Added lab5a

--- lab5a.py
+++ lab5a.py
@@ -11,14 +11,13 @@ def read_file_list(file_name):
     f = open(file_name, 'r')
     read_data = f.read()
     f.close()
-    read_data.split('\n') 
-    list_of_lines = read_data.split('\n') 
-    empty_lines = [] 
-    for line in list_of_lines:
-        if line != '': 
-            empty_lines.append(line) 
-    return empty_lines
+    list_of_lines = read_data.split('\n') #split the data into a list of lines
+    filter_lines = [] #initialize a empty list to store non-empty lines
 
+    for line in list_of_lines:
+        if line != '': #if the line is not empty, add it to the empty lines list
+            filter_lines.append(line)
+    return filter_lines
 
 if __name__ == '__main__':
     file_name = 'data.txt'
