4048b050e2bf1d49c49bddfc81aeaf83e4e72a6c
Ali Teimouri <ateimouri1@myseneca.ca>
2024-07-12 11:01:58 -0400

    Added lab5a

--- lab5a.py
+++ lab5a.py
@@ -9,9 +9,16 @@ def read_file_string(file_name):
 
 def read_file_list(file_name):
     f = open(file_name, 'r')
-    lines = [line.strip() for line in f] #read each line of the file
+    read_data = f.read()
     f.close()
-    return lines
+    read_data.split('\n') 
+    list_of_lines = read_data.split('\n') 
+    empty_lines = [] 
+    for line in list_of_lines:
+        if line != '': 
+            empty_lines.append(line) 
+    return empty_lines
+
 
 if __name__ == '__main__':
     file_name = 'data.txt'
